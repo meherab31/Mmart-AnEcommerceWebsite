bb68c980312f48375d005416b0ebe6d8b9a5058a
MD Meherab Hasan <58429543+meherab31@users.noreply.github.com>
2024-01-16 00:58:06 +0600

    product fields

--- resources/views/admin/addproduct.blade.php
+++ resources/views/admin/addproduct.blade.php
@@ -3,16 +3,38 @@
   <head>
     @include('admin.css')
     <style>
-        .div_center{
+        .div_center {
             text-align: center;
             padding-top: 20px;
         }
-        .h1_font{
+        .h1_font {
             font-size: 45px;
             padding-bottom: 40px;
         }
-
-
+        .form-container {
+            display: inline-block;
+            text-align: left;
+        }
+        label {
+            width: 200px;
+            display: inline-block;
+            text-align: left;
+        }
+        input {
+            margin-bottom: 10px;
+        }
+        input::placeholder{
+            font-size: 70%;
+        }
+        select {
+            margin-bottom: 10px;
+        }
+        .submit{
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+        }
     </style>
   </head>
   <body>
@@ -28,35 +50,42 @@
        <div class="content-wrapper" >
         <div class="div_center">
            <h2 class="h2_font">Add Product</h2>
-            <div>
-                <label>Product Title</label>
-                <input type="text" name="title" placeholder="Write the Product Title" >
+           <div class="form-container">
+                <div>
+                    <label>Product Title</label>
+                    <input type="text" name="title" placeholder="Write the Product Title">
+                </div>
+                <div>
+                    <label>Prodcut Description</label>
+                    <input type="text" name="description" placeholder="Write the Product Description">
+                </div>
+                <div>
+                    <label>Prodcut Quantity</label>
+                    <input type="number" name="quantity" min="0" placeholder="Place Product Quantity">
+                </div>
+                <div>
+                    <label>Prodcut Category</label>
+                    <select>
+                        <option value="" selected="">Add a category</option>
+                        <option>Shirt</option>
+                    </select>
+                </div>
+                <div>
+                    <label>Prodcut Price</label>
+                    <input type="number" name="price" placeholder="Place the product price">
+                </div>
+                <div>
+                    <label>Prodcut Discount price</label>
+                    <input type="number" name="discount_price" placeholder="Write the Discounted Product Price">
+                </div>
+                <div>
+                    <label>Prodcut Image</label>
+                    <input type="text" name="image" placeholder="Upload the Product Image">
+                </div>
+                <div class="submit">
+                    <input type="submit">
+                </div>
             </div>
-            <div>
-                <label>Prodcut Title</label>
-                <input type="text" name="description" placeholder="Write the Product Title" >
-            </div>
-            <div>
-                <label>Prodcut Title</label>
-                <input type="text" name="quantity" placeholder="Write the Product Title" >
-            </div>
-            <div>
-                <label>Prodcut Title</label>
-                <input type="text" name="category" placeholder="Write the Product Title" >
-            </div>
-            <div>
-                <label>Prodcut Title</label>
-                <input type="text" name="price" placeholder="Write the Product Title" >
-            </div>
-            <div>
-                <label>Prodcut Title</label>
-                <input type="text" name="discount_price" placeholder="Write the Product Title" >
-            </div>
-            <div>
-                <label>Prodcut Title</label>
-                <input type="text" name="image" placeholder="Write the Product Title" >
-            </div>
-
         </div>
        </div>
      </div>
