84a2f16fa728d879681b2dc79da577020a9785d7
MD Meherab Hasan <58429543+meherab31@users.noreply.github.com>
2024-01-31 01:24:08 +0600

    1/2 showing product data

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\Product;
 use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
@@ -25,7 +26,7 @@ class HomeController extends Controller
     }
 
     public function index(){
-
-        return view('home.userpage');
+        $product=Product::all();
+        return view('home.userpage', compact('product'));
     }
 }
--- resources/views/home/userpage.blade.php
+++ resources/views/home/userpage.blade.php
@@ -53,13 +53,7 @@
       <!-- footer start -->
       @include('home.footer')
       <!-- footer end -->
-      <div class="cpy_">
-         <p class="mx-auto">© 2021 All Rights Reserved By <a href="https://html.design/">Free Html Templates</a><br>
 
-            Distributed By <a href="https://themewagon.com/" target="_blank">ThemeWagon</a>
-
-         </p>
-      </div>
       <!-- jQery -->
       <script src="home/js/jquery-3.4.1.min.js"></script>
       <!-- popper js -->
