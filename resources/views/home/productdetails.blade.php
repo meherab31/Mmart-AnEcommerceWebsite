5c399a5ed7788855d74a60c1a093373e0d8ccf39
MD Meherab Hasan <58429543+meherab31@users.noreply.github.com>
2024-02-03 16:55:04 +0600

    Product Details Page almost Design

--- resources/views/home/productdetails.blade.php
+++ resources/views/home/productdetails.blade.php
@@ -1,75 +1,89 @@
 <!DOCTYPE html>
-<html>
-   <head>
+<html lang="en">
+
+<head>
     <base href="/public">
-      <!-- Basic -->
-      <meta charset="utf-8" />
-      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-      <!-- Mobile Metas -->
-      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-      <!-- Site Metas -->
-      <meta name="keywords" content="" />
-      <meta name="description" content="" />
-      <meta name="author" content="" />
-      <link rel="shortcut icon" href="images/favicon.png" type="">
-      <title>{{ $product->title }}</title>
-      <!-- bootstrap core css -->
-      <link rel="stylesheet" type="text/css" href="home/css/bootstrap.css" />
-      <!-- font awesome style -->
-      <link href="home/css/font-awesome.min.css" rel="stylesheet" />
-      <!-- Custom styles for this template -->
-      <link href="home/css/style.css" rel="stylesheet" />
-      <!-- responsive style -->
-      <link href="home/css/responsive.css" rel="stylesheet" />
-   </head>
-   <body>
-      <div class="hero_area">
-         <!-- header section strats -->
-        @include('home.header')
-         <!-- end header section -->
-         <div class="col-sm-6 col-md-4 col-lg-4" style="margin: auto; width: 50px; padding:30px">
-            <div class="box">
-               <div class="img-box">
-                  <img src="product/{{ $product->image }} " height="250px" width="250px" alt="">
-               </div>
-               <div class="detail-box">
-                  <h5>
-                   {{ $product->title }}
-                  </h5>
-                  <br>
-                  @if ($product->discount_price!=null)
-                  <h6 style="color:red">
-                   ${{ $product->discount_price }}
-                  </h6>
+    <!-- Basic -->
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <!-- Mobile Metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <!-- Site Metas -->
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <link rel="shortcut icon" href="images/favicon.png" type="">
+    <title>{{ $product->title }}</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" type="text/css" href="home/css/bootstrap.css" />
+    <!-- Font Awesome CSS -->
+    <link href="home/css/font-awesome.min.css" rel="stylesheet" />
+    <!-- Custom styles for this template -->
+    <link href="home/css/style.css" rel="stylesheet" />
+    <!-- Responsive styles -->
+    <link href="home/css/responsive.css" rel="stylesheet" />
+</head>
 
-                  <h6 style="text-decoration: line-through; color:rgba(4, 34, 0, 0.829)">
-                   ${{ $product->price }}
-                  </h6>
+<body>
+    <!-- Header -->
+    @include('home.header')
 
-                  @else
-                  <h6 style="color:rgb(4, 34, 0)">
-                   ${{ $product->price }}
-                  </h6>
-                  @endif
-
-                  <p>
-                    {{ $product->description }}
-                  </p>
-               </div>
+    <!-- Product Details -->
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="product-image">
+                    <img src="product/{{ $product->image }}" class="img-fluid" alt="{{ $product->title }}">
+                </div>
             </div>
-         </div>
-      </div>
-      <!-- footer start -->
-      @include('home.footer')
-      <!-- footer end -->
+            <div class="col-lg-6">
+                <div class="product-details">
+                    <h2>{{ $product->title }}</h2>
 
-      <!-- jQery -->
-      <script src="home/js/jquery-3.4.1.min.js"></script>
-      <!-- popper js -->
-      <script src="home/js/popper.min.js"></script>
-      <!-- bootstrap js -->
-      <script src="home/js/bootstrap.js"></script>
-      <!-- custom js -->
-      <script src="home/js/custom.js"></script>
-   </body>
+                    <div class="price">
+                        <strong>
+                        @if ($product->discount_price != null)
+                        <span style="color:red">${{ $product->discount_price }}</span>
+                        <span style="text-decoration: line-through; color:rgba(4, 34, 0, 0.829)">{{ $product->price }}</span>
+                        @else
+                        <span style="color:rgb(4, 34, 0)">${{ $product->price }}</span>
+                        @endif
+                        </strong>
+                    </div>
+
+                    <p class="description">{{ $product->description }}</p>
+
+                    <!-- Add to Cart and Buy Now buttons -->
+                    <div class="btn-container mt-3">
+                        <button class="btn btn-primary" onclick="addToCart()">Add to Cart</button>
+                        <button class="btn btn-success" onclick="buyNow()">Buy Now</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Footer -->
+    @include('home.footer')
+
+    <!-- Scripts -->
+    <script src="home/js/jquery-3.4.1.min.js"></script>
+    <script src="home/js/popper.min.js"></script>
+    <script src="home/js/bootstrap.js"></script>
+    <script src="home/js/custom.js"></script>
+
+    <!-- Add to Cart and Buy Now functionality -->
+    <script>
+        function addToCart() {
+            // Add your logic to handle adding the product to the cart
+            alert('Product added to cart!');
+        }
+
+        function buyNow() {
+            // Add your logic to handle the "Buy Now" functionality
+            alert('Redirecting to checkout for immediate purchase!');
+        }
+    </script>
+</body>
+
 </html>
